2f4424a48990cdca95f7dc8a32c57df599ee3ec7
devmoaid1 <moaidmohamed1312@gmail.com>
2024-10-13 04:41:24 +0300

    print linked list

--- data_structures/linked_list.dart
+++ data_structures/linked_list.dart
@@ -6,7 +6,7 @@ class Node<T> {
 }
 
 class LinkedList<T> {
-  void addItem(Node<T> head, Node<T> node) {
+  void addItem(Node<T> head, T item) {
     // set pointer to head
     // if pointer`s next is null set pointer`s next to given node.
     // otherwise move pointer to next node ,repeat
@@ -15,6 +15,15 @@ class LinkedList<T> {
     while (pointer.next != null) {
       pointer = pointer.next!;
     }
-    pointer.next = node;
+    pointer.next = Node<T>(item);
+  }
+
+  void printList(Node<T> head) {
+    Node<T> pointer = head;
+    while (pointer.next != null) {
+      print(pointer.data);
+      pointer = pointer.next!;
+    }
+    print(pointer.data);
   }
 }
--- main.dart
+++ main.dart
@@ -3,12 +3,10 @@ import 'data_structures/linked_list.dart';
 void main() {
   LinkedList<int> numbersLinkedList = LinkedList<int>();
 
-  final thirdNum = Node<int>(2);
-  final secondNum = Node<int>(1);
   final head = Node<int>(0);
 
-  numbersLinkedList.addItem(head, secondNum);
-  numbersLinkedList.addItem(head, thirdNum);
+  numbersLinkedList.addItem(head, 1);
+  numbersLinkedList.addItem(head, 2);
 
-  print("head next is ${secondNum.next!.data} ");
+  numbersLinkedList.printList(head);
 }
